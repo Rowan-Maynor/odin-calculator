427810c00b97d6197a5748aba9b1ba3105ee0357
Rowan Maynor <rowanmaynor@gmail.com>
2024-09-20 16:07:16 -0400

    Create opBtns click events
    Its still buggy, but I need to fix some CSS issues
    before I go back to this.

--- javascript.js
+++ javascript.js
@@ -4,17 +4,48 @@ let operation;
 
 const displayText = document.querySelector("#displayText");
 const numBtns = document.querySelectorAll(".numBtn");
+const opBtns = document.querySelectorAll(".opBtn");
 
 numBtns.forEach(btn => {
     btn.addEventListener("click", ()=>{
         if(displayText.textContent.length > 11){
-            displayText.textContent = "Input too long!"
+            displayText.textContent = "Input too long!";
         } else if(displayText.textContent == "0") {
             displayText.textContent = btn.textContent;
         }  else {
             displayText.textContent += btn.textContent;
         }
-    })
+    });
+});
+
+opBtns.forEach(btn => {
+    console.log(btn.id);
+    switch(btn.id){
+        case "btnAdd":
+            btn.addEventListener("click", () => {
+                input1 = displayText.textContent;
+                operation = "add";
+                displayText.textContent = "0";
+            });
+        case "btnSub":
+            btn.addEventListener("click", () => {
+                input1 = displayText.textContent;
+                operation = "sub";
+                displayText.textContent = "0";
+            });
+        case "btnMult":
+            btn.addEventListener("click", () => {
+                input1 = displayText.textContent;
+                operation = "mult";
+                displayText.textContent = "0";
+            });
+        case "btnDivide":
+            btn.addEventListener("click", () => {
+                input1 = displayText.textContent;
+                operation = "divide";
+                displayText.textContent = "0";
+        });
+    }
 });
 
 function add(num1, num2) {
@@ -37,9 +68,9 @@ function operate(num1, num2, operation){
     switch (operation){
         case "add":
             return add(num1, num2);
-        case "subtract":
+        case "sub":
             return subtract(num1, num2);
-        case "multiply":
+        case "mult":
             return multiply(num1, num2);
         case "divide":
             return divide(num1, num2);
