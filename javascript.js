9335681ec6f6a7c7210f762369baca541c100e3e
Rowan Maynor <rowanmaynor@gmail.com>
2024-09-20 18:17:03 -0400

    Check for divide by zero

--- javascript.js
+++ javascript.js
@@ -139,7 +139,11 @@ function multiply(num1, num2) {
 };
 
 function divide(num1, num2) {
-    return +num1 / +num2;
+    if(num1 == "0" || num2 == "0"){
+        return "ERROR";
+    } else {
+        return +num1 / +num2;
+    }
 };
 
 function operate(num1, num2, operation){
