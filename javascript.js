d769638bbaa8051bedac43d759966527718a831c
Rowan Maynor <rowanmaynor@gmail.com>
2024-09-20 16:59:35 -0400

    Fix select statement
    Did not originaly have breaks which was causing all code
    to execute regardless of case

--- javascript.js
+++ javascript.js
@@ -19,49 +19,73 @@ numBtns.forEach(btn => {
 });
 
 opBtns.forEach(btn => {
-    console.log(btn.id);
-    switch(btn.id){
+    let id = btn.id;
+    switch(id){
         case "btnAdd":
             btn.addEventListener("click", () => {
                 input1 = displayText.textContent;
                 operation = "add";
                 displayText.textContent = "0";
             });
+            break;
         case "btnSub":
             btn.addEventListener("click", () => {
                 input1 = displayText.textContent;
                 operation = "sub";
                 displayText.textContent = "0";
             });
+            break;
         case "btnMult":
             btn.addEventListener("click", () => {
                 input1 = displayText.textContent;
                 operation = "mult";
                 displayText.textContent = "0";
             });
+            break;
         case "btnDivide":
             btn.addEventListener("click", () => {
                 input1 = displayText.textContent;
                 operation = "divide";
                 displayText.textContent = "0";
-        });
+            });
+            break;
+        case "btnClear":
+            btn.addEventListener("click", () => {
+                displayText.textContent = "0";
+            });
+            break;
+        case "btnEql":
+            btn.addEventListener("click", () => {
+                if(input1 == undefined){
+                    console.log("No input");
+                } else if (operation == undefined) {
+                    console.log("No Operation");
+                } else {
+                    input2 = displayText.textContent;
+                    displayText.textContent = operate(input1, input2, operation);
+                }
+            });
+            break;
+        default:
+            console.log("Button id not found");
+            break;
     }
 });
 
 function add(num1, num2) {
-    return num1 + num2;
+    return +num1 + +num2;
 };
   
 function subtract(num1, num2) {
-    return num1 - num2;
+    return +num1 - +num2;
 };
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return +num1 * +num2;
 };
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return +num1 / +num2;
 };
 
 function operate(num1, num2, operation){
@@ -76,5 +100,6 @@ function operate(num1, num2, operation){
             return divide(num1, num2);
         default:
             console.log("Please enter a valid operation (+-*/)");
+            break;
     }
 }
